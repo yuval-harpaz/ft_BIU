c947c544a5fb4515e6e35bab5c3ccfbf44905ffd
Yuval Harpaz <yuvharpaz@gmail.com>
2012-03-15 12:16:33 +0200

    making a fix for new fieldtrip packs to suit BIU data

--- matlab/files/fixFT4BIU.m
+++ matlab/files/fixFT4BIU.m
@@ -1,5 +1,28 @@
 function fixFT4BIU
-display(['1. remove fieldtrip*/external/spm2* and fieldtrip*/external/spm8* from the path'])
-display('2. replace fieldtrip*/read_trigger.m with ~/ft_BIU/matlam/ft_files/read_trigger.m');
-display('3. for beamformer_sam.m replace progress with ft_progress a few times')
+% this function meant to fix newly downloaded fieldtrip packages, after 
+% adding them to matlab path, for bar ilan university use.
+
+ft=which('ft_definetrial');
+k = findstr('ft_definetrial.m', ft);
+path2ft=ft(1:k-1);
+cd(path2ft)
+if ~exist('./ftFixed4BIU','file')
+    if ~exist('fileio/private/read_triggerOrig.m','file')
+        movefile('fileio/private/read_trigger.m','fileio/private/read_triggerOrig.m');
+        copyfile('~/ft_BIU/matlab/ft_files/read_triggerBIU.m','fileio/private/read_trigger.m')
+    end
+    if exist('external/spm2','dir')
+        movefile('external/spm2','~/Dektop/')
+        display('I put external/spm2 on ~/Desktop')
+    end
+    if exist('external/spm8','dir')
+        movefile('external/spm8','~/Dektop/')
+        display('I put external/spm8 on ~/Desktop')
+    end
+    display('for beamformer_sam.m replace progress with ft_progress a few times')
+    copyfile('~/ft_BIU/matlab/files/ftFixed4BIU','ftFixed4BIU')
+else
+    display('it seems thif ft package was fixed already')
+end
+    
 
