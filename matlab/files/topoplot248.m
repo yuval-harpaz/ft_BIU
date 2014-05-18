0d539c726a2e74d5f0d93c978421ba17f91dc105
yuval <yuval@bar-ilan>
2014-05-18 13:12:26 +0300

    old change

--- matlab/files/topoplot248.m
+++ matlab/files/topoplot248.m
@@ -1,14 +1,18 @@
 function topoplot248(vec,cfg,sortedChans);
 %% reading weights
 % topoplot of 248 values
+if ~exist('sortedChans','var')
+    sortedChans=false;
+end
+if ~exist ('cfg','var')
+    cfg=[];
+end
 load ~/ft_BIU/matlab/plotwts
 if sortedChans
     for chani=1:248
         wts.label{chani,1}=['A',num2str(chani)];
     end
 end
-if ~exist ('cfg','var')
-    cfg=[];
-end
+
 wts.avg(:,1)=vec;
 figure;ft_topoplotER(cfg,wts);
