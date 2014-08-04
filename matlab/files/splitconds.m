5233761ef01e18cd963e3e25d9780bfc2bfb5d10
yuval-harpaz <yuvharpaz@gmail.com>
2014-08-04 09:23:43 +0300

    Update splitconds.m
    vadim corrected a bug

--- matlab/files/splitconds.m
+++ matlab/files/splitconds.m
@@ -26,8 +26,8 @@ if (i<100);
     ndata=rmfield(ndata,'time');
     cnt=1;
     for i=1:size(t,1);
-        if ~isempty(find(data.cfg.trl==t(i,1))) && t(i,4)==cfg.cond; %#ok<EFIND>
-            ti=find(data.cfg.trl==t(i,1));
+        if ~isempty(find(data.cfg.trl(:,1)==t(i,1))) && t(i,4)==cfg.cond; %#ok<EFIND>
+            ti=find(data.cfg.trl(:,1)==t(i,1));
             ndata.cfg.trl(cnt,1:size(t,2))=t(i,:);
             ndata.trial{1,cnt}=data.trial{1,ti};
             ndata.time{1,cnt}=data.time{1,ti};
