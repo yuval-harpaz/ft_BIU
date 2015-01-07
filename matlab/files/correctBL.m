2d834503f7dcea22515c068fbd355119579db9fd
yuval <yuval@bar-ilan>
2015-01-07 11:38:39 +0200

    gitUpdate

--- matlab/files/correctBL.m
+++ matlab/files/correctBL.m
@@ -1,27 +1,38 @@
-function dataBL=correctBL(data,blcwindow)
+function data=correctBL(data,blcwindow)
 % dataBL=correctBL(dataica,[-0.2 0]);
-dataBL=data;
 
-if ~isfield(dataBL,'avg')
-    trialLength=size(dataBL.trial{1,1},2);
-    if ~exist('blcwindow','var')
-        firstSamp=1;
-        lastSamp=trialLength;
-    else
-        firstSamp=nearest(dataBL.time{1,1},blcwindow(1));
-        lastSamp=nearest(dataBL.time{1,1},blcwindow(2));
-    end
-    for trial=1:size(dataBL.trial,2)
-        m=mean(dataBL.trial{1,trial}(:,firstSamp:lastSamp),2);
-        for i=1:size(m,1); m(i,2:trialLength)=m(i,1);end
-        dataBL.trial{1,trial}=dataBL.trial{1,trial}-m;
-    end
-else
-    firstSamp=nearest(dataBL.time,blcwindow(1));
-    lastSamp=nearest(dataBL.time,blcwindow(2));
-    trialLength=size(dataBL.avg,2);
-    m=mean(dataBL.avg(:,firstSamp:lastSamp),2);
-    for i=1:size(m,1); m(i,2:trialLength)=m(i,1);end
-    dataBL.avg=dataBL.avg-m;
+if isfield(data,'avg')
+    dataType='avg';
+    time=data.time;
+elseif isfield(data,'individual')
+    dataType='Gavg';
+    time=data.time;
+elseif isfield(data,'trial')
+    dataType='trial';
+    time=data.time{1};
 end
+
+trialLength=length(time);
+if ~exist('blcwindow','var')
+    firstSamp=1;
+    lastSamp=trialLength;
+else
+    firstSamp=nearest(time,blcwindow(1));
+    lastSamp=nearest(time,blcwindow(2));
+end
+switch dataType
+    case 'trials'
+        for trial=1:size(data.trial,2)
+            m=mean(data.trial{1,trial}(:,firstSamp:lastSamp),2);
+            m=repmat(m,1,trialLength);
+            data.trial{1,trial}=data.trial{1,trial}-m;
+        end
+    case 'avg'
+        m=mean(data.avg(:,firstSamp:lastSamp),2);
+        m=repmat(m,1,trialLength);
+        data.avg=data.avg-m;
+    case 'Gavg'
+        m=mean(data.individual(:,:,firstSamp:lastSamp),3);
+        m=repmat(squeeze(m),[1,1,trialLength]);
+        data.individual=data.individual-m;
 end
