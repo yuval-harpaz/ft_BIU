6c6212f2bc73a9edebecd69499794785ddc23532
yuval <yuval@bar-ilan>
2015-03-15 10:11:57 +0200

    fix ft 4 BIU

--- matlab/files/fixFT4BIU.m
+++ matlab/files/fixFT4BIU.m
@@ -12,19 +12,26 @@ if ~exist('./ftFixed4BIU','file')
         copyfile('~/ft_BIU/matlab/ft_files/read_triggerBIU.m','fileio/private/read_trigger.m')
     end
     if exist('external/spm2','dir')
-        movefile('external/spm2','~/Dektop/')
-        display('I put external/spm2 on ~/Desktop')
+        if ~exist('~/Dektop/outOfFT','dir')
+            mkdir('~/Dektop/outOfFT')
+        end
+        movefile('external/spm2','~/Dektop/outOfFT/')
+        display('I put external/spm2 on ~/Desktop/outOfFT/')
     end
     if exist('external/spm8','dir')
-        movefile('external/spm8','~/Dektop/')
+        if ~exist('~/Dektop/outOfFT','dir')
+            mkdir('~/Dektop/outOfFT')
+        end
+        movefile('external/spm8','~/Dektop/outOfFT/')
         display('I put external/spm8 on ~/Desktop/outOfFT/')
     end
     display('for beamformer_sam.m replace progress with ft_progress a few times')
-    copyfile('~/ft_BIU/matlab/files/ftFixed4BIU','ftFixed4BIU')
-    if ~exist('./fileio/private/read_4D_hdr_old.m','file')
-        movefile('fileio/private/read_4D_hdr.m','fileio/private/read_4D_hdr_old.m')
-        copyfile('~/ft_BIU/matlab/ft_files/read_ft_hdr_BIU1.m','fileio/private/read_4D_hdr.m')
+    
+    if ~exist('./fileio/private/read_4d_hdr_old.m','file')
+        movefile('fileio/private/read_4d_hdr.m','fileio/private/read_4d_hdr_old.m')
+        copyfile('~/ft_BIU/matlab/ft_files/read_4d_hdr_BIU1.m','fileio/private/read_4d_hdr.m')
     end
+    copyfile('~/ft_BIU/matlab/files/ftFixed4BIU','ftFixed4BIU')
 else
     display('it seems that this ft package was fixed already')
 end
